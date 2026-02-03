32dca10cf99fa4c067a4c39183d1eb3cb1de7f55
Salazar99 <mr.nicoletti99@gmail.com>
2026-02-03 16:12:11 +0100

    Removed fault-based qualification functions (no longer supported). Removed z3 dependencies in Slam code (required only for fault-based qualification).
    Kept z3 install script (STLSAT requires it).

--- src/miner/modules/include/propertyQualification/qualifier/Qualifier.hh
+++ src/miner/modules/include/propertyQualification/qualifier/Qualifier.hh
@@ -35,10 +35,6 @@ public:
                                            Trace *trace) override;
 
 private:
-  /** \brief parse a trace containing a fault
-   */
-  Trace *parseFaultyTrace(const std::string &ftStr);
-
   /** \brief evaluate the given metric on the given assertion
    */
   double evaluate(Assertion &a, Metric &m);
@@ -47,14 +43,7 @@ private:
   double scoreFunction(double x, FunctionParameter &fp);
   void printAssertions(Context &context, std::vector<Assertion *> &assertions,
                        Trace *trace);
-  /** \brief find fault coverage
-   */
-  void faultBasedQualification(std::vector<Assertion *> selected, Trace *trace);
-  void fbqUsingFaultyTraces(std::vector<Assertion *> &selected);
-  /** \brief faults are inserted direclty on the trace
-   */
-  void fbqUsingFaultOnTraceMode(std::vector<Assertion *> &selected,
-                                Trace *trace, size_t &nFaults);
+
   void dumpAssToFile(Context &context, Trace *trace,
                      std::vector<Assertion *> &assertions);
 
@@ -95,10 +84,6 @@ private:
    */
   void loadParams();
 
-  /** \brief run the sat solver to find the minimum number of assertions covering the maximum number of faults
-   */
-  std::vector<size_t> getCoverageSet();
-
   /** \brief returns all the assertions were ant != con
    */
   std::vector<Assertion *>
@@ -116,22 +101,11 @@ private:
 
   size_t _traceLength;
 
-  ///keeps the ids of the assertions covering the maximum numbers of faults
-  std::vector<size_t> _coverageSet;
-
   std::unordered_map<size_t, FunctionParameter> _functionParams;
-  /** \brief filter redundant assertions, quikly allbeit less precisely
-   */
   /** \brief max value to calibrate the function in the ranking
    * values go from 1 (range is 0.1-10) to 90 (range is 0.9-1)
    */
   size_t _maxParamIndex = 0;
-  ///maps the assertion to the faults they cover
-  std::unordered_map<size_t, std::vector<size_t>> _aToF;
-  ///maps covered faults to the assertion covering them
-  std::unordered_map<size_t, std::vector<size_t>> _fToA;
-  ///used in fault-on-trace mode to keep the relation between a fault and corresponding bit a variable
-  std::unordered_map<size_t, std::string> _fToVar;
 };
 
 } // namespace slam
